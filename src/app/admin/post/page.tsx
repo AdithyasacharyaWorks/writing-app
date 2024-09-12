c518942aeecae0ea8dc881b6732c31d259875e07
adithya <adithyasacharya292@gmail.com>
2024-09-12 23:10:08 +0530

    changed tje favicon and fiexed small css

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -5,7 +5,7 @@ import PostForm from '@/components/custom/Post_form';
 
 const Page = () => {
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+    <div className="">
       <div className="w-full max-w-2xl">
         <h1 className="text-4xl font-bold text-white p-2 sticky top-0 bg-black mb-8 text-center">Create New Post</h1>
         <PostForm/>
